af62e79e03336dfb7c331336a7440d26767f87b9
leonid <chemestru@gmail.com>
2023-07-26 13:21:03 +0300

    v2

--- main/reg_resiver_server.go
+++ main/reg_resiver_server.go
@@ -42,9 +42,9 @@ func main() {
 
 func regSite() {
 
-   // Запускаем прослушивание на :8080 порту
+   // Запускаем прослушивание на :8080 порту   http://localhost:8080/regApp
    http.HandleFunc("/regApp", siteRequest)
-   log.Fatal(http.ListenAndServe(":8080", nil))
+   log.Fatal(http.ListenAndServe(":8080", addCorsHeaders(http.DefaultServeMux)))
 }
 
 func regResiver() {
@@ -59,60 +59,60 @@ func regHandleRequest(w http.ResponseWriter, r *http.Request) {
     // Если пришел POST запрос
     if r.Method == "POST" {
         // Если получен нужный заголовок
-	if r.URL.Path == "/regResiverServer" {
-	    // Получаем тело запроса
-	    body, err := ioutil.ReadAll(r.Body)
-	    if err != nil {
-                http.Error(w, "Failed to read request body", http.StatusBadRequest)
-	        return
-	    }
+        if r.URL.Path == "/regResiverServer" {
+            // Получаем тело запроса
+            body, err := ioutil.ReadAll(r.Body)
+            if err != nil {
+                    http.Error(w, "Failed to read request body", http.StatusBadRequest)
+                return
+            }
 
-        // Создаем структуру для парсинга
-	    var person Person
+            // Создаем структуру для парсинга
+            var person Person
 
-	    // Парсим тело запроса в труктуру
-	    err = json.Unmarshal(body, &person)
-	    if err != nil {
-	        http.Error(w, "Failed to parse JSON", http.StatusBadRequest)
-	        return
-	     }
+            // Парсим тело запроса в труктуру
+            err = json.Unmarshal(body, &person)
+            if err != nil {
+                http.Error(w, "Failed to parse JSON", http.StatusBadRequest)
+                return
+            }
 
-        // Печатаем данные из запроса
-	    fmt.Println("Received JSON:", string(body))
-	    fmt.Println("Login:", person.Login)
-	    fmt.Println("Password:", person.Password)
-	    fmt.Println("Favorite game:", person.FavoriteGame)
+            // Печатаем данные из запроса
+            fmt.Println("Received JSON:", string(body))
+            fmt.Println("Login:", person.Login)
+            fmt.Println("Password:", person.Password)
+            fmt.Println("Favorite game:", person.FavoriteGame)
 
-        var answer Answer
-        dbAnswer := sql_connector.ReadUserInfo(person.Login)
-        if dbAnswer != "" {
+            var answer Answer
+            dbAnswer := sql_connector.ReadUserInfo(person.Login)
+            if dbAnswer != "" {
 
-            answer.Text = "You have been registration before"
-	        answer.FavoriteGame = "Your favourute game is "+dbAnswer
+                answer.Text = "You have been registration before"
+                answer.FavoriteGame = "Your favourute game is "+dbAnswer
 
+            } else {
+                // Запись в базу данных
+                sql_connector.AddData(person.Login, person.Password, person.FavoriteGame)
+
+                answer.Text = "Successfull registration!"
+                answer.FavoriteGame = "Cool game!"
+            }
+            
+
+            // Парсим в json структуру ответа
+            responseJSON, err := json.Marshal(answer)
+            if err != nil {
+                http.Error(w, "Failed to generate JSON response", http.StatusInternalServerError)
+                return
+            }
+
+            // Конфигурируем ответ
+            w.Header().Set("Content-Type", "application/json")
+            // Отправляем ответ
+            w.Write(responseJSON)
         } else {
-            // Запись в базу данных
-	        sql_connector.AddData(person.Login, person.Password, person.FavoriteGame)
-
-            answer.Text = "Successfull registration!"
-	        answer.FavoriteGame = "Cool game!"
+            http.NotFound(w, r)
         }
-	    
-
-        // Парсим в json структуру ответа
-	    responseJSON, err := json.Marshal(answer)
-	    if err != nil {
-	        http.Error(w, "Failed to generate JSON response", http.StatusInternalServerError)
-	        return
-	    }
-
-        // Конфигурируем ответ
-	    w.Header().Set("Content-Type", "application/json")
-	    // Отправляем ответ
-	    w.Write(responseJSON)
-	} else {
-	    http.NotFound(w, r)
-	}
     } else {
         http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
     }
